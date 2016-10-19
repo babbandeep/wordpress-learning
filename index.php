76f04ba0ea3eba58f4f2e4eaac92f470e215c440
babbandeep <babangurdaspur@gmail.com>
2016-10-19 16:03:49 +0530

    updated

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 get_header();
 
+if(have_posts()){
+	while(have_posts()){
+			the_post();
+			the_content();
+	}
+}
+
+if(get_post_meta(get_queried_object_id(), 'testing', true)){
+	echo get_post_meta(get_queried_object_id(), 'testing', true);
+}
+
 get_footer();
 
 ?>
